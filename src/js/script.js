b17103cb71791a3c2a6504e1cdc34b7883e84a44
faisal almalki <faisal.almalki101001@gmail.com>
2018-10-24 21:40:42 +0300

    initial commit

---
+++ src/js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    $('[data-toggle="tooltip"]').tooltip();
+
+    $('[data-add-to-cart]').click(function(e){
+        alert('أضيف المنتج إلى عربة الشراء')
+            e.stopPropagation();
+
+    });
+
+    $('.product-option input[type="radio"]').change(function() {
+        $(this).parents('.product-option').siblings().removeClass('active');
+        $(this).parents('.product-option').addClass('active');
+    });
+
+});
